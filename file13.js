5d266a55d1f5c0729d137496ae028c0d58e9d6a8
Lyba <faisallyba06@gmail.com>
2024-01-17 19:50:24 +0500

    Logic-2

--- file13.js
+++ file13.js
@@ -1,8 +1,18 @@
 //Arrays Today
+//Logic # 1
 
 let num=[1,2,3,4,5,6,7,8];          //array num
 
-for(let i=0 ; i<=num.length-1 ; i++)
+for(let i=0 ; i<=num.length-1 ; i++)   //displaying through (length-1)
 {
     console.log(num[i]);
 }
+console.log("Logic 1 Completed");
+//Logic # 2
+
+let numbers= [1,3,5,2,8,7,9];
+for (let i=0 ; i<7 ; i++)              //Displaying Simple(through index)
+{
+    console.log(numbers[i]);
+}
+console.log("Logic 2 Completed");
