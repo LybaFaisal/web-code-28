54ee0c4bab7af3060f6e726a2df34c51adcb28ba
Lyba <faisallyba06@gmail.com>
2024-01-18 13:39:56 +0500

    ModifyingArrayElement

--- file13.js
+++ file13.js
@@ -16,3 +16,15 @@ for (let i=0 ; i<7 ; i++)              //Displaying Simple(through index)
     console.log(numbers[i]);
 }
 console.log("Logic 2 Completed");
+
+//Logic # 3
+
+let arr1=[2,4,6,7,10,12,14,16];
+
+arr1[3]=8;                   //modifying array element
+
+for(let i=0 ; i<7 ; i++)     //for(let i=0 ; i<arr1.length-1 ; i++)
+{
+    console.log(arr1[i]);
+}
+console.log("Logic 3 completed");
